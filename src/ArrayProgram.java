2e805f5a66b1d70380133d9d180e372989fd8247
Cby <1042172087@qq.com>
2020-11-18 23:56:37 +0800

    test

--- src/ArrayProgram.java
+++ src/ArrayProgram.java
@@ -1,51 +1,46 @@
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import static org.apache.commons.lang3.RandomUtils.nextInt;
 import static org.apache.commons.lang3.StringUtils.abbreviate;
 
 public class ArrayProgram {
 
     /*
-     * 给整型数组初始化，若n=0，则建立一个长度为1的数组，对于数组的值，则调用RandomUtils的nextInt方法进行随机赋值
+     * 给整型数组初始化，若n=0，则建立一个长度为1的数组，对于数组的值，则分别赋值为n的i次方
      */
     public int[] init_array(int n){
         int[] array;
         if(n==0){
             array=new int[1];
-            array[0]= nextInt()%100;
+            array[0]= 1;
         }
         else {
             array=new int[n];
             for (int i=0;i<n;i++){
-                array[i]= nextInt()%100;
+                array[i]= (int)Math.pow(n,i);
             }
         }
         return array;
     }
 
     /*
-     * 给字符串初始化，若length不为0，则调用RandomUtils的randomAlphanumeric方法进行随机赋值
+     * 给字符串初始化，将所有小写字母转为大写
      */
-    public String init_string(int length){
-        String str;
-        if(length==0)
-            str="";
-        else{
-            str=org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric(length);
-        }
-        return str;
+    public String init_string(String string){
+        string=StringUtils.upperCase(string);
+        return string;
     }
 
     /*
      * 将整型数组转为字符串
      */
-    public String intToString(int[] array){
+    public String arrayToString(int[] array){
         StringBuilder stringBuilder=new StringBuilder();
         int len=array.length;//获取数组长度
-        int i=len-1;
-        while(i>=0){
+        int i=0;
+        while(i<len){
             stringBuilder.append(array[i]);
-            i--;
+            i++;
         }
         return stringBuilder.toString();
     }
@@ -53,12 +48,14 @@ public class ArrayProgram {
     /*
      * 将长度超过10的字符串进行缩略输出，输出长度为5
      */
-    void abbrevPrint(String str){
+    String abbrevPrint(String str){
         int len=str.length();
+        String string;
         if(len>=10)
-            System.out.println(abbreviate(str,"...",5));
+            string=abbreviate(str,"...",5);
         else
-            System.out.println(str);
+            string=str;
+        return string;
     }
 
     /*
@@ -67,4 +64,53 @@ public class ArrayProgram {
     String stringTail(String str){
         return StringUtils.appendIfMissingIgnoreCase(str,"cby");
     }
+
+    /*
+     * 将数组转为整数
+     */
+    int arrayToInt(int[] array){
+        int len=array.length;
+        int num=0;
+        for(int i=0;i<len;i++){
+            num*=10;
+            num+=array[i];
+        }
+        return num;
+    }
+
+    /*
+     * 判断两个数组是否相等
+     */
+    public boolean is_same(int[] array1,int [] array2) {
+        int len = ArrayUtils.getLength(array1);
+        int i=0;
+        while(i<len) {
+            if(array1[i]==array2[i])
+                i++;
+            else
+                break;
+        }
+        if(i==len||i==ArrayUtils.getLength(array2))
+            return true;
+        else
+            return false;
+    }
+
+    /*
+    public boolean is_same(int[] array1,int[] array2){
+        System.out.println("not the same");
+        return false;
+    }*/
+
+    /*
+     * 将两个数组合并
+     */
+    public int[] addAll(int[] array1,int[] array2) {
+        boolean flag = is_same(array1,array2);
+        if(flag) return array1;
+        else {
+            return ArrayUtils.addAll(array1, array2);
+        }
+    }
+
 }
---
+++ src/ArrayProgramTest.java
@@ -0,0 +1,81 @@
+
+import static org.junit.Assert.*;
+
+public class ArrayProgramTest {
+
+    ArrayProgram program=new ArrayProgram();
+
+    @org.junit.Test
+    public void init_array() {
+        int[] arr1=program.init_array(3);
+        int[] arr2={1,3,9};
+        assertArrayEquals(arr1,arr2);
+    }
+
+    @org.junit.Test
+    public void init_string() {
+        String str1="soiHIFS";
+        String str2="SOIHIFS";
+        assertEquals(program.init_string(str1),str2);
+    }
+
+    @org.junit.Test
+    public void arrayToString() {
+        int[] arr=program.init_array(4);
+        String str="141664";
+        assertEquals(str,program.arrayToString(arr));
+    }
+
+    @org.junit.Test
+    public void abbrevPrint() {
+        String str="abbreviate";
+        String str1="ab...";
+        assertEquals(str1,program.abbrevPrint(str));
+    }
+
+    @org.junit.Test
+    public void stringTail() {
+        String str1="osdhg";
+        String str2="osdhgcby";
+        assertEquals(str2,program.stringTail(str1));
+    }
+
+    @org.junit.Test
+    public void arrayToInt(){
+        int[] array={2,56,36};
+        int num=796;
+        assertEquals(num,program.arrayToInt(array));
+    }
+
+    @org.junit.Test
+    public void test_is_same(){
+        int[] arr1={1,2,3};
+        int[] arr2={1,2,3};
+        int[] arr3={2,4,5};
+        assertEquals(true,program.is_same(arr1,arr2));
+        assertEquals(false,program.is_same(arr1,arr3));
+    }
+
+    @org.junit.Test
+    public void test_toString(){
+        int[] array=program.init_array(5);
+        String str=program.arrayToString(array);
+        boolean flag=true;
+        assertEquals(5,array.length);
+        if(!str.equals("1525125625"))
+            flag=false;
+        assertEquals(true,flag);
+    }
+
+    @org.junit.Test
+    public void test_addAll(){
+        int[] arr1={1,2,3};
+        int[] arr2={3,5};
+        int[] arr3={1,2,3,3,5};
+        int[] arr4=program.addAll(arr1,arr2);
+        assertEquals(arr3.length,arr4.length);
+        int i=0;
+        for(;i<arr3.length;i++)
+            assertEquals(arr3[i],arr4[i]);
+    }
+}
