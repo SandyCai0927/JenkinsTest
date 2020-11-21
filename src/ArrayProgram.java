1bfb49890f3dfa3b5da6f630dba7c1fa9ff8945a
Cby <1042172087@qq.com>
2020-11-21 09:22:41 +0800

    correct

--- src/ArrayProgram.java
+++ src/ArrayProgram.java
@@ -5,9 +5,6 @@ import static org.apache.commons.lang3.StringUtils.abbreviate;
 
 public class ArrayProgram {
 
-    /*
-     * 给整型数组初始化，若n=0，则建立一个长度为1的数组，对于数组的值，则分别赋值为n的i次方
-     */
     public int[] init_array(int n){
         int[] array;
         if(n==0){
@@ -23,17 +20,11 @@ public class ArrayProgram {
         return array;
     }
 
-    /*
-     * 给字符串初始化，将所有小写字母转为大写
-     */
     public String init_string(String string){
         string=StringUtils.upperCase(string);
         return string;
     }
 
-    /*
-     * 将整型数组转为字符串
-     */
     public String arrayToString(int[] array){
         StringBuilder stringBuilder=new StringBuilder();
         int len=array.length;//获取数组长度
@@ -45,9 +36,6 @@ public class ArrayProgram {
         return stringBuilder.toString();
     }
 
-    /*
-     * 将长度超过10的字符串进行缩略输出，输出长度为5
-     */
     String abbrevPrint(String str){
         int len=str.length();
         String string;
@@ -58,16 +46,10 @@ public class ArrayProgram {
         return string;
     }
 
-    /*
-     * 如果字符串不是以cby结尾，则在字符串末尾添加cby
-     */
     String stringTail(String str){
         return StringUtils.appendIfMissingIgnoreCase(str,"cby");
     }
 
-    /*
-     * 将数组转为整数
-     */
     int arrayToInt(int[] array){
         int len=array.length;
         int num=0;
@@ -78,9 +60,6 @@ public class ArrayProgram {
         return num;
     }
 
-    /*
-     * 判断两个数组是否相等
-     */
     public boolean is_same(int[] array1,int [] array2) {
         int len = ArrayUtils.getLength(array1);
         int i=0;
@@ -102,9 +81,6 @@ public class ArrayProgram {
         return false;
     }*/
 
-    /*
-     * 将两个数组合并
-     */
     public int[] addAll(int[] array1,int[] array2) {
         boolean flag = is_same(array1,array2);
         if(flag) return array1;
